4869fe150ee0cfb905ad72b06ba1a78c20231480
Tuarender <tuarender005@gmail.com>
2015-08-15 14:47:29 +0700

    update js for responsive banner

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,5 +1,5 @@
 <div class="slideContainer">
-  <div class="fotorama" data-width="100%" data-height="100%" data-fit="cover" data-loop="true">
+  <div class="fotorama"  data-width="100%" data-height="100%" data-fit="cover" data-loop="true">
     <img src="images/banner/sample1.jpg">
     <img src="images/banner/sample2.jpg">
     <img src="images/banner/sample3.jpg">
