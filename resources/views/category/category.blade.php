fa2a0b91ecc9aa2eca5387a5465cacabbe1cf269
Iso Junior <isojocker@gmail.com>
2015-08-15 22:19:39 +0700

    Update Query
    Update Query

--- resources/views/category/category.blade.php
+++ resources/views/category/category.blade.php
@@ -9,8 +9,8 @@
                     @if($i==5)
                        <br><?php $i=1; ?>
                     @endif
-        <div class="col-lg-3"> <a href="pdf/{!! $data->CATEGORY_PATH_PDF !!}" target="_blank"><img src="cover/{!!  $data->CATEGORY_COVER_PIC !!}.jpg"> </a>
-           <b> <p>{!! $data->CATEGORY_NAME  !!}</p></b> </div>
+        <div class="col-lg-3"> <a href="pdf/{!! $data['CATEGORY_PATH_PDF'] !!}" target="_blank"><img src="cover/{!!  $data['CATEGORY_COVER_PIC'] !!}.jpg"> </a>
+           <b> <p>{!! $data['CATEGORY_NAME']  !!}</p></b> </div>
                         <?php $i++; ?>
 
         @endforeach
