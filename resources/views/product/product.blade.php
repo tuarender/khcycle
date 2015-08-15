9dbfa08711ce9455e977478dfb7367a683b98697
Tuarender <tuarender005@gmail.com>
2015-08-15 22:20:20 +0700

    update product

--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -2,11 +2,27 @@
 @section('content')
 
 <div class="productContainer">
+	<div class="brandContainer">
+		<?php
+		  if(isset($brand)){
+		    print_r($brand[0]);
+		    echo "<ul class='brandList'>";
+		    for($i=0;$i<count($brand);$i++){
+		    	echo "<li>";
+		    	echo "<a href='#'>";
+		    	echo "<div class='brandDiv'><img src='images/brand/product/sample".$brand[$i]["BRAND_ID"].".jpg'>";
+		    	echo "</div>";
+		    	echo "</a>";
+		    	echo "</li>";
+		    }
+		    echo "</ul>";
+		  }
+		?>
+	</div>
+	<div>
+		Product Detail
+	</div>
 </div>
-<?php
-  if(isset($brand)){
-    print_r($brand);
-  }
-?>
+
 @endsection
 @stop
