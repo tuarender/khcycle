2dad78110e957e349873bcadf23e357000bd1b6c
Tuarender <tuarender005@gmail.com>
2015-08-16 12:39:28 +0700

    update resonsive for product view

--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -2,22 +2,24 @@
 @section('content')
 
 <div class="productContainer">
-	<div class="brandContainer">
-		<?php
-		  if(isset($brand)){
-		    print_r($brand[0]);
-		    echo "<ul class='brandList'>";
-		    for($i=0;$i<count($brand);$i++){
-		    	echo "<li>";
-		    	echo "<a href='#'>";
-		    	echo "<div class='brandDiv'><img src='images/brand/product/sample".$brand[$i]["BRAND_ID"].".jpg'>";
-		    	echo "</div>";
-		    	echo "</a>";
-		    	echo "</li>";
-		    }
-		    echo "</ul>";
-		  }
-		?>
+	<div class="brandWrapper">
+		<div class="brandContainer">
+			<?php
+			  if(isset($brand)){
+			    //print_r($brand[0]);
+			    echo "<ul class='brandList'>";
+			    for($i=0;$i<count($brand);$i++){
+			    	echo "<li>";
+			    	echo "<a href='#'>";
+			    	echo "<div class='brandDiv'><img src='images/brand/product/sample".$brand[$i]["BRAND_ID"].".jpg'>";
+			    	echo "</div>";
+			    	echo "</a>";
+			    	echo "</li>";
+			    }
+			    echo "</ul>";
+			  }
+			?>
+		</div>
 	</div>
 	<div>
 		Product Detail
