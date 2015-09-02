a9216601cdb7104432fcc4dbf22b74570779ea4e
Iso Junior <isojocker@gmail.com>
2015-09-02 22:00:54 +0700

    UpdateLogin
    UpdateLogin

--- resources/views/member/login.blade.php
+++ resources/views/member/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="contactcontainer">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
+            <div class="col-sm-8 col-sm-offset-2">
                 @include('partials.flashmessage')
                 <div class="panel panel-primary">
                     <div class="panel-heading">เข้าสู่ระบบสมาชิก</div>
@@ -22,8 +22,8 @@
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
                             <div class="form-group @if ($errors->has('kh_username')) has-error @endif">
-                                <label class="col-md-4 control-label">Username</label>
-                                <div class="col-md-5">
+                                <label class="col-sm-4 control-label">Username</label>
+                                <div class="col-sm-5">
                                     <input type="text" class="form-control" name="kh_username" value="{{ old('kh_username') }}">
                                     @if($errors->has('kh_username')) <p class="help-block">{{$errors->first('kh_username')}}</p>@endif
                                 </div>
@@ -31,19 +31,19 @@
                             </div>
 
                             <div class="form-group @if ($errors->has('kh_password')) has-error @endif">
-                                <label class="col-md-4 control-label">Password</label>
-                                <div class="col-md-5">
+                                <label class="col-sm-4 control-label">Password</label>
+                                <div class="col-sm-5">
                                     <input type="password" class="form-control" name="kh_password">
                                     @if($errors->has('kh_password')) <p class="help-block">{{$errors->first('kh_password')}}</p>@endif
                                 </div>
                             </div>
 
                             <div class="form-group">
-                                <div class="col-md-9 control-label"><a href="forgetpassword">ลืมรหัสผ่าน</a></div>
+                                <div class="col-sm-9 control-label"><a href="forgetpassword">ลืมรหัสผ่าน</a></div>
                             </div>
 
                             <div class="form-group">
-                                <div class="col-md-5 col-md-offset-4">
+                                <div class="col-sm-5 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary">
                                         เช้าสู่ระบบ
                                     </button>
--- resources/views/member/register.blade.php
+++ resources/views/member/register.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="contactcontainer">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
+            <div class="col-sm-8 col-sm-offset-2">
+                <div class="panel panel-primary">
                     <div class="panel-heading">Register</div>
                     <div class="panel-body">
                         @if (count($errors) > 0)
@@ -23,24 +23,24 @@
                         <form class="form-horizontal" role="form" method="post" action="register">
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
                             <div class="form-group @if ($errors->has('member_username')) has-error @endif" >
-                                <label class="col-md-4 control-label"><font color="red">*</font>Username</label>
-                                <div class="col-md-5">
+                                <label class="col-sm-4 control-label"><font color="red">*</font>Username</label>
+                                <div class="col-sm-5">
                                     <input type="text" class="form-control" name="member_username" value="{{ old('member_username') }}" >
                                     @if($errors->has('member_username')) <p class="help-block">{{$errors->first('member_username')}}</p>@endif
                                 </div>
                             </div>
 
                             <div class="form-group @if ($errors->has('member_password')) has-error @endif">
-                                <label class="col-md-4 control-label"><font color="red">*</font>Password</label>
-                                <div class="col-md-5">
+                                <label class="col-sm-4 control-label"><font color="red">*</font>Password</label>
+                                <div class="col-sm-5">
                                     <input type="password" class="form-control" name="member_password" >
                                     @if($errors->has('member_password')) <p class="help-block">{{$errors->first('member_password')}}</p>@endif
                                 </div>
                             </div>
 
                             <div class="form-group @if ($errors->has('member_password_confirmation')) has-error @endif">
-                                <label class="col-md-4 control-label"><font color="red">*</font>Confirm Password</label>
-                                <div class="col-md-5">
+                                <label class="col-sm-4 control-label"><font color="red">*</font>Confirm Password</label>
+                                <div class="col-sm-5">
                                     <input type="password" class="form-control" name="member_password_confirmation" >
                                     @if($errors->has('member_password_confirmation')) <p class="help-block">{{$errors->first('member_password_confirmation')}}</p>@endif
                                 </div>
@@ -48,8 +48,8 @@
 
 
                             <div class="form-group @if ($errors->has('member_email')) has-error @endif">
-                                <label class="col-md-4 control-label"><font color="red">*</font>E-Mail Address</label>
-                                <div class="col-md-5">
+                                <label class="col-sm-4 control-label"><font color="red">*</font>E-Mail Address</label>
+                                <div class="col-sm-5">
                                     <input type="email" class="form-control" name="member_email" value="{{ old('member_email') }}" >
                                     @if($errors->has('member_email')) <p class="help-block">{{$errors->first('member_email')}}</p>@endif
                                 </div>
@@ -57,16 +57,16 @@
 
 
                             <div class="form-group @if ($errors->has('member_email_confirmation')) has-error @endif">
-                                <label class="col-md-4 control-label"><font color="red">*</font>Confirm Email</label>
-                                <div class="col-md-5">
+                                <label class="col-sm-4 control-label"><font color="red">*</font>Confirm Email</label>
+                                <div class="col-sm-5">
                                     <input type="email" class="form-control" name="member_email_confirmation" value="{{ old('member_email_confirmation') }}" >
                                     @if($errors->has('member_email_confirmation')) <p class="help-block">{{$errors->first('member_email_confirmation')}}</p>@endif
                                 </div>
                             </div>
 
                             <div class="form-group @if ($errors->has('member_name')) has-error @endif">
-                                <label class="col-md-4 control-label"><font color="red">*</font>ชื่อ-นามสกุล</label>
-                                <div class="col-md-5">
+                                <label class="col-sm-4 control-label"><font color="red">*</font>ชื่อ-นามสกุล</label>
+                                <div class="col-sm-5">
                                     <input type="text" class="form-control" name="member_name" value="{{ old('member_name') }}" >
                                     @if($errors->has('member_name')) <p class="help-block">{{$errors->first('member_name')}}</p>@endif
                                 </div>
@@ -74,8 +74,8 @@
 
 
                             <div class="form-group @if ($errors->has('member_tel')) has-error @endif">
-                                <label class="col-md-4 control-label"><font color="red">*</font>เบอร์โทรศัพท์</label>
-                                <div class="col-md-5">
+                                <label class="col-sm-4 control-label"><font color="red">*</font>เบอร์โทรศัพท์</label>
+                                <div class="col-sm-5">
                                     <input type="text" class="form-control" name="member_tel" value="{{ old('member_tel') }}" >
                                     @if($errors->has('member_tel')) <p class="help-block">{{$errors->first('member_tel')}}</p>@endif
                                 </div>
@@ -83,43 +83,43 @@
 
 
                             <div class="form-group @if ($errors->has('member_address')) has-error @endif">
-                                <label class="col-md-4 control-label"><font color="red">*</font>ที่อยู่</label>
-                                <div class="col-md-5">
+                                <label class="col-sm-4 control-label"><font color="red">*</font>ที่อยู่</label>
+                                <div class="col-sm-5">
                                     <textarea rows="5"  class="form-control" name="member_address" id="{{ old('member_address') }}"></textarea>
                                     @if($errors->has('member_address')) <p class="help-block">{{$errors->first('member_address')}}</p>@endif
                                 </div>
                             </div>
 
                             <div class="form-group @if ($errors->has('member_weight')) has-error @endif">
-                                <label class="col-md-4 control-label"><font color="red">*</font>น้ำหนัก</label>
-                                <div class="col-md-4">
+                                <label class="col-sm-4 control-label"><font color="red">*</font>น้ำหนัก</label>
+                                <div class="col-sm-4">
                                     <input type="text"  class="form-control" onkeypress="validate(event)" name="member_weight" maxlength="5" value="{{ old('member_weight') }}"  >
                                     @if($errors->has('member_weight')) <p class="help-block">{{$errors->first('member_weight')}}</p>@endif
                                 </div>
-                                <label class="col-md-1 control-label">KG</label>
+                                <label class="col-sm-1 control-label">KG</label>
                             </div>
 
                             <div class="form-group @if ($errors->has('member_height')) has-error @endif">
-                                <label class="col-md-4 control-label"><font color="red">*</font>ส่วนสูง</label>
-                                <div class="col-md-4">
+                                <label class="col-sm-4 control-label"><font color="red">*</font>ส่วนสูง</label>
+                                <div class="col-sm-4">
                                     <input type="text"  class="form-control" onkeypress="validate(event)" name="member_height" maxlength="5" value="{{ old('member_height') }}" >
                                     @if($errors->has('member_height')) <p class="help-block">{{$errors->first('member_height')}}</p>@endif
                                 </div>
-                                <label class="col-md-1 control-label">CM</label>
+                                <label class="col-sm-1 control-label">CM</label>
                             </div>
 
                             <div class="form-group @if ($errors->has('member_shoe')) has-error @endif">
-                                <label class="col-md-4 control-label"><font color="red">*</font>เบอร์รองเท้า</label>
-                                <div class="col-md-4">
+                                <label class="col-sm-4 control-label"><font color="red">*</font>เบอร์รองเท้า</label>
+                                <div class="col-sm-4">
                                     <input type="text"  class="form-control" onkeypress="validate(event)" name="member_shoe" maxlength="4" value="{{ old('member_shoe') }}" >
                                     @if($errors->has('member_shoe')) <p class="help-block">{{$errors->first('member_shoe')}}</p>@endif
                                 </div>
-                                <label class="col-md-1 control-label">EU</label>
+                                <label class="col-sm-1 control-label">EU</label>
                             </div>
 
 
                             <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
+                                <div class="col-sm-6 col-sm-offset-4">
                                     <button type="submit" class="btn btn-primary">
                                         ยืนยัน
                                     </button>
