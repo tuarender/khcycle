f52f88fe565958fed93f8ce08beeaacd138d2f6a
Iso Junior <isojocker@gmail.com>
2015-09-03 22:11:48 +0700

    Update Contact in MainHome
    Update Contact in MainHome

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -37,6 +37,26 @@
           <img src="images/loading.gif" style="display: block;margin-left: auto;margin-right: auto;">
         </div>
     </div>
+
+  <div class="contactContainer">
+    <div class="jumbotron">
+    <div class="row">
+      <div class="col-lg-4">
+        <b>KHCYCLE BIKE STUDIO</b><br>
+
+        <b>เลขที่ 29 ถนนประเสริฐมนูญกิจ แขวงคลองกุ่ม กรุงเทพฯ 10240</b><br>
+        <b>โทร: 0-2510-1906</b><br>
+        <b>Email:Sales@khcycle.com</b><br>
+
+        คลิ๊กเพื่อดูแผนที่ขนาดใหญ่<br>
+        <div id="googleMap" style="width:400px;height:300px;"></div>
+      </div>
+      <div class="col-lg-8">
+        <img src="images/contact/contact.png"/>
+      </div>
+    </div>
+    </div>
+  </div>
 @endsection
 @section('scripts')
   <script type="text/javascript">
@@ -87,6 +107,33 @@
         }
       }
     }
+
+  </script>
+  <script src="http://maps.googleapis.com/maps/api/js"></script>
+  <hr>
+  <script>
+    var myCenter=new google.maps.LatLng(13.8082871,100.6528724);
+    var marker;
+
+    function initialize()
+    {
+      var mapProp = {
+        center:myCenter,
+        zoom:15,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+      };
+
+      var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+      var marker=new google.maps.Marker({
+        position:myCenter,
+        animation:google.maps.Animation.BOUNCE
+      });
+
+      marker.setMap(map);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
   </script>
 @endsection
 @stop
